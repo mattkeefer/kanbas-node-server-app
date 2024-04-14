539d8ef5d61ede9f85ef7fcc397a0663e39ad98f
Matt Keefer <mrkeefer8@gmail.com>
2024-04-14 16:56:56 -0400

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import UserRoutes from "./Kanbas/users/routes.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
     cors({
